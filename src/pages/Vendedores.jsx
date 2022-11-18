14268479c8f58a82dcb0434ab2c5bb631d4cad61
Marcelo Souto <marcelosouto676@gmail.com>
2022-11-17 23:41:39 -0300

    loading na pagina de vendedores

--- src/pages/Vendedores.jsx
+++ src/pages/Vendedores.jsx
@@ -1,28 +1,33 @@
-import React from "react";
-import axios from "axios";
-import UserInfo from "../components/User/UserInfo";
-import UserProduct from "../components/User/UserProduct";
-import UserModal from "../components/User/UserModal";
-import styles from "./Vendedores.module.css";
-import ButtonPrimary from "../components/Button/ButtonPrimary";
-import Head from "../components/global/Head";
+import React from 'react';
+import axios from 'axios';
+import UserInfo from '../components/User/UserInfo';
+import UserProduct from '../components/User/UserProduct';
+import UserModal from '../components/User/UserModal';
+import styles from './Vendedores.module.css';
+import ButtonPrimary from '../components/Button/ButtonPrimary';
+import Head from '../components/global/Head';
+import Loading from '../components/Loading'
 
 /* <Input value={value} onChange={handleChange} id='buscador' place='ID' />
  <Button text='Buscar' /> */
 
 const Vendedores = () => {
   let id = 2;
-  const [value, setValue] = React.useState("");
   const [response, setResponse] = React.useState(null);
   const [showModal, setShowModal] = React.useState(false);
   const [modalInfo, setModalInfo] = React.useState();
+  const [loading, setLoading] = React.useState(null)
 
   React.useEffect(() => {
+    setLoading(true);
     axios
       .get(
-        `https://fake-server-company.herokuapp.com/sellers/${id}?_embed=products`
+        `https://fake-server-company.herokuapp.com/sellers/${id}?_embed=products`,
       )
-      .then(({ data }) => setResponse(data));
+      .then(({ data }) => {
+        setLoading(false)
+        setResponse(data);
+      });
   }, []);
 
   function handleClick(id) {
@@ -35,6 +40,7 @@ const Vendedores = () => {
     }
   }
 
+  if (loading) return <Loading />
   return (
     <section>
       <Head title="Painel do Usuario" />
